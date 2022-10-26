03fe79393667aa492227f1b0758207722ce9b37c
hasi1997 <72153296+hasi1997@users.noreply.github.com>
2022-10-26 18:43:29 +0530

    Update Multi.java

--- Multi.java
+++ Multi.java
@@ -2,10 +2,10 @@ public class Multi {
 
     public static void main(String[] args) {
 
-        float fnum = 10.2f;
-        float snum = 3.5f;
+        float firstnumber = 10.2f;
+        float secondnumber = 3.5f;
 
-        float product = fnum * snum;
+        float product = firstnumber * secondnumber;
 
         System.out.println("The product is: " + product);
     }
